2e4264a89b45b47872df5be812fd5e21b71e49e1
TanakaZuze <99540354+TanakaZuze@users.noreply.github.com>
2026-02-17 15:36:56 +0200

    backend-initialize
    -configured project on https://start.spring.io/

---
+++ backend/ping_system/src/main/java/dev/tanaka/ping_system/PingSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.tanaka.ping_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingSystemApplication.class, args);
+	}
+
+}
---
+++ backend/ping_system/src/test/java/dev/tanaka/ping_system/PingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tanaka.ping_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
